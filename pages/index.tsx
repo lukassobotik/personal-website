a5522717873b0881c6f0839ed24e41ad8ddfe969
PuckyEU <osmset@gmail.com>
2023-01-05 21:43:01 +0100

    Featured Projects Basic Display

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,58 @@ export default function Home() {
           </div>
         </div>
 
+        <h2 className={styles.featured}>
+          Featured Projects
+        </h2>
+        <div className={styles.grid}>
+          <a href={""} className={styles.project}>
+            <h3 className={inter.className}>
+              MyFavMovies <span>-&gt;</span>
+            </h3>
+            <h5 className={inter.className}>
+              JavaScript, React, Firebase
+            </h5>
+            <p className={inter.className}>
+              MyFavMovies is a web application that allows you to search for movies and add them to your favorites.
+            </p>
+          </a>
+          <a href={""} className={styles.project}>
+            <h3 className={inter.className}>
+              myQuotes <span>-&gt;</span>
+            </h3>
+            <h5 className={inter.className}>
+              Java, Android, Firebase
+            </h5>
+            <p className={inter.className}>
+              myQuotes is an Android application where you can Share, Discover and Save New Quotes!
+            </p>
+          </a>
+          <a href={""} className={styles.project}>
+            <h3 className={inter.className}>
+              VŠEM Change Notifier <span>-&gt;</span>
+            </h3>
+            <h5 className={inter.className}>
+              Java, JavaMail, JSoup
+            </h5>
+            <p className={inter.className}>
+                VŠEM Schedule Change Notifier is a Java application that notifies you about changes in the schedule of your classes.
+            </p>
+          </a>
+          <a href={""} className={styles.project}>
+            <h3 className={inter.className}>
+              Repeaty <span>-&gt;</span>
+            </h3>
+            <h5 className={inter.className}>
+              Java, Android, SQLite
+            </h5>
+            <p className={inter.className}>
+              Repeaty is an android application to Track and Manage your Habits.
+            </p>
+          </a>
+        </div>
+        <h2 className={styles.featured}>
+          External Links
+        </h2>
         <div className={styles.grid}>
           <a href="https://github.com/PuckyEU/puckyeu.github.io" className={styles.card}>
             <h2 className={inter.className}>
