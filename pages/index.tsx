df00a9c6b9edfb1aa444768288010ce1c0856ec9
PuckyEU <osmset@gmail.com>
2023-01-14 12:27:27 +0100

    More Detailed Description

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,19 @@ import {Inter} from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Navbar from "./navbar";
 import Link from "next/link";
-import GetRandomLine from "../GetRandomLine";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  console.log(GetRandomLine());
+  function calculateAge() {
+    let birthdate = new Date(2007, 5, 10);
+    let currentTime = new Date().getTime();
+    let birthDateTime = new Date(birthdate).getTime();
+    let ageInYears = (currentTime - birthDateTime) / 31_536_000_000;
+    return Math.floor(ageInYears);
+  }
+
   return (
     <>
       <Head>
@@ -22,12 +28,89 @@ export default function Home() {
             Hi, I am <span className={styles.name}>Lukáš</span>, a <span className={styles.name}>Full-Stack </span>developer.
           </h1>
         </div>
-        <h2 className={styles.featured}>
-          Overview
-        </h2>
-        <div className={styles.overview}>
-          <p>I am a student at Akademie VŠEM studying economics. Programming is my passion and it will most likely be my full time job after I graduate, so I am open to any job offers or internships.</p>
-          <p>I am currently learning Java, and I have had a bit of experience with JavaScript and C# as well.</p>
+        <div className={styles.overview_section}>
+          <h2 className={styles.featured}>
+            About Me
+          </h2>
+          <div className={styles.overview}>
+            <p>I am a student at Akademie VŠEM studying economics. Programming is my passion and it will most likely be my full time job after I graduate, so I am open to any job offers or internships. I am currently learning Java, and I have had a bit of experience with JavaScript and C# as well.</p>
+          </div>
+          <h2 className={styles.featured}>
+            Personal Details
+          </h2>
+          <div className={styles.personal_details}>
+            <p className={styles.name}>Name:</p>
+            <p>Lukáš Sobotík</p>
+            <p className={styles.name}>Age:</p>
+            <p>{calculateAge()}</p>
+            <p className={styles.name}>Job Status:</p>
+            <p>Looking for a job</p>
+          </div>
+          <h2 className={styles.featured}>
+            Skills
+          </h2>
+          <div className={styles.skills}>
+            <Link href="https://www.java.com/" className={styles.skill}>
+              <h3 className={inter.className}>
+                Java <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://firebase.google.com/" className={styles.skill}>
+              <h3 className={inter.className}>
+                Firebase <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://git-scm.com/" className={styles.skill}>
+              <h3 className={inter.className}>
+                Git <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://www.markdownguide.org/" className={styles.skill}>
+              <h3 className={inter.className}>
+                Markdown <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://www.javascript.com/" className={styles.skill}>
+              <h3 className={inter.className}>
+                JavaScript <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://reactjs.org/" className={styles.skill}>
+              <h3 className={inter.className}>
+                React <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://www.arduino.cc/" className={styles.skill}>
+              <h3 className={inter.className}>
+                Arduino <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://www.raspberrypi.org/" className={styles.skill}>
+              <h3 className={inter.className}>
+                Raspberry Pi <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://www.blackmagicdesign.com/products/davinciresolve" className={styles.skill}>
+              <h3 className={inter.className}>
+                Davinci Resolve <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://www.adobe.com/products/photoshop.html" className={styles.skill}>
+              <h3 className={inter.className}>
+                Photoshop <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://www.blender.org/" className={styles.skill}>
+              <h3 className={inter.className}>
+                Blender <span>-&gt;</span>
+              </h3>
+            </Link>
+            <Link href="https://learn.microsoft.com/en-us/dotnet/csharp/" className={styles.skill}>
+              <h3 className={inter.className}>
+                C# <span>-&gt;</span>
+              </h3>
+            </Link>
+          </div>
         </div>
         <h2 className={styles.featured}>
           Featured Projects
