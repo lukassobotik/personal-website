ade47a3a167e198d6dc4f769911c5b99123a19c4
PuckyEU <osmset@gmail.com>
2023-02-10 21:43:57 +0100

    Schedule Change Notifier Description

--- pages/project/vsem-schedule-change-notifier.tsx
+++ pages/project/vsem-schedule-change-notifier.tsx
@@ -24,8 +24,14 @@ export default function VsemScheduleChangeNotifier() {
                             VŠEM Schedule Change Notifier is a Java application that notifies you about changes in the schedule of your classes. The application is built using Java, JavaMail and Jsoup. It is deployed on a Raspberry Pi and runs on a Cron job.
                         </p>
                         <p>
+                            I basically made this application for myself, because I was tired of checking the schedule of my classes on the VŠEM website every day. I wanted to be notified about changes in the schedule of my classes. But decided to make it public, so that other students can use it as well.
+                        </p>
+                        <p>
                             The application is running a Cron job that runs every 1 hour. The Cron job checks the schedule of the classes on the VŠEM website. If there is a change in the schedule, the application sends you an email with the new schedule.
                             On the Raspberry Pi, the application logs all the changes in the schedule, every run of the application and the emails that were sent. The application also logs the errors that occurred during the execution of the application.
+                        </p>
+                        <p>
+                            I made my own logging system, because it&apos;d be a waste of resources to use a logging library for such a small application.
                         </p>
                         <div className={styles.project_link}>
                             <a href="https://github.com/PuckyEU/vsem-schedule-change-notifier" className={styles.skill}>Source Code</a>
