9ba66b9fc4eb0242df9547e864b218526bac25f1
PuckyEU <osmset@gmail.com>
2023-02-21 21:17:47 +0100

    #9 fix

--- pages/featuredProjects.tsx
+++ pages/featuredProjects.tsx
@@ -1,80 +1,79 @@
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import {FadeLeft, FadeRight} from "../animations";
+import {useEffect} from "react";
+import 'aos/dist/aos.css'
+
+const AOS = require('aos');
 
 export default function FeaturedProjects() {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <>
             <div className={styles.featured_projects}>
-                <FadeLeft>
-                    <Link href="/project/MyFavMovies" data-aos="fade-right" className={styles.featured_project}>
-                        <div className={styles.featured_project_description}>
-                            <h2>MyFavMovies</h2>
-                            <h4>
-                                JavaScript, React, Firebase
-                            </h4>
-                            <p>
-                                MyFavMovies is a website made for finding movies and adding them to your favorites.
-                            </p>
-                            <p className={styles.extra_description}>
-                                It uses the The Movie Database API to fetch the movies.
-                            </p>
-                        </div>
-                        <Image src="/images/MyFavMovies/MyFavMovies.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
-                    </Link>
-                </FadeLeft>
-                <FadeRight>
-                    <Link href="/project/myQuotes" data-aos="fade-right" className={styles.featured_project}>
-                        <Image src="/images/myQuotes/myQuotes.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
-                        <div className={styles.featured_project_description}>
-                            <h2>MyQuotes</h2>
-                            <h4>
-                                Java, Android, Firebase
-                            </h4>
-                            <p>
-                                myQuotes is an Android application where you can Share, Discover and Save New Quotes!
-                            </p>
-                            <p className={styles.extra_description}>
-                                This was my first completed Android application that I published on the Google Play Store.
-                            </p>
-                        </div>
-                    </Link>
-                </FadeRight>
-                <FadeLeft>
-                    <Link href="/project/vsem-schedule-change-notifier" data-aos="fade-right" className={styles.featured_project}>
-                        <div className={styles.featured_project_description}>
-                            <h2>VŠEM Schedule Change Notifier</h2>
-                            <h4>
-                                Java, JavaMail, JSoup
-                            </h4>
-                            <p>
-                                VŠEM Schedule Change Notifier is an app that notifies you about changes in the schedule of your classes.
-                            </p>
-                            <p className={styles.extra_description}>
-                                The application is built using Java, JavaMail and Jsoup. It is deployed on a Raspberry Pi and runs on a Cron job.
-                            </p>
-                        </div>
-                        <Image src="/images/schedule-change-notifier/VŠEM.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
-                    </Link>
-                </FadeLeft>
-                <FadeRight>
-                    <Link href="/project/Repeaty" data-aos="fade-right" className={styles.featured_project}>
-                        <Image src="/images/Repeaty/Repeaty.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
-                        <div className={styles.featured_project_description}>
-                            <h2>Repeaty</h2>
-                            <h4>
-                                Java, Android, SQLite
-                            </h4>
-                            <p>
-                                Repeaty is an android application to Track and Manage your Habits.
-                            </p>
-                            <p className={styles.extra_description}>
-                                Even though it was never finished, it was my first Android application and I learned a lot about Android development.
-                            </p>
-                        </div>
-                    </Link>
-                </FadeRight>
+                <Link href="/project/MyFavMovies" data-aos="fade-right" className={styles.featured_project}>
+                    <div className={styles.featured_project_description}>
+                        <h2>MyFavMovies</h2>
+                        <h4>
+                            JavaScript, React, Firebase
+                        </h4>
+                        <p>
+                            MyFavMovies is a website made for finding movies and adding them to your favorites.
+                        </p>
+                        <p className={styles.extra_description}>
+                            It uses the The Movie Database API to fetch the movies.
+                        </p>
+                    </div>
+                    <Image src="/images/MyFavMovies/MyFavMovies.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                </Link>
+                <Link href="/project/myQuotes" data-aos="fade-left" className={styles.featured_project}>
+                    <Image src="/images/myQuotes/myQuotes.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                    <div className={styles.featured_project_description}>
+                        <h2>MyQuotes</h2>
+                        <h4>
+                            Java, Android, Firebase
+                        </h4>
+                        <p>
+                            myQuotes is an Android application where you can Share, Discover and Save New Quotes!
+                        </p>
+                        <p className={styles.extra_description}>
+                            This was my first completed Android application that I published on the Google Play Store.
+                        </p>
+                    </div>
+                </Link>
+                <Link href="/project/vsem-schedule-change-notifier" data-aos="fade-right" className={styles.featured_project}>
+                    <div className={styles.featured_project_description}>
+                        <h2>VŠEM Schedule Change Notifier</h2>
+                        <h4>
+                            Java, JavaMail, JSoup
+                        </h4>
+                        <p>
+                            VŠEM Schedule Change Notifier is an app that notifies you about changes in the schedule of your classes.
+                        </p>
+                        <p className={styles.extra_description}>
+                            The application is built using Java, JavaMail and Jsoup. It is deployed on a Raspberry Pi and runs on a Cron job.
+                        </p>
+                    </div>
+                    <Image src="/images/schedule-change-notifier/VŠEM.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                </Link>
+                <Link href="/project/Repeaty" data-aos="fade-left" className={styles.featured_project}>
+                    <Image src="/images/Repeaty/Repeaty.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                    <div className={styles.featured_project_description}>
+                        <h2>Repeaty</h2>
+                        <h4>
+                            Java, Android, SQLite
+                        </h4>
+                        <p>
+                            Repeaty is an android application to Track and Manage your Habits.
+                        </p>
+                        <p className={styles.extra_description}>
+                            Even though it was never finished, it was my first Android application and I learned a lot about Android development.
+                        </p>
+                    </div>
+                </Link>
             </div>
         </>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 import Head from "next/head";
 import FeaturedProjects from "./featuredProjects";
 import {useEffect} from "react";
-import {FadeUp0, FadeUp100, FadeUp200, FadeUp300} from "../animations";
+import 'aos/dist/aos.css'
+
+const AOS = require('aos');
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +31,7 @@ export default function Home() {
         },
         false
     );
+    AOS.init();
   })
 
   return (
@@ -191,49 +194,38 @@ export default function Home() {
           External Links
         </h2>
         <div className={styles.grid}>
-          <FadeUp0>
-            <a href="https://github.com/PuckyEU/puckyeu.github.io" className={styles.card}>
-              <h2 className={inter.className}>
-                Source Code <span>-&gt;</span>
-              </h2>
-              <p className={inter.className}>
-                Here you can find all of my source code for this website. If you have any questions or suggestions, feel free to contact me!
-              </p>
-            </a>
-          </FadeUp0>
-
-          <FadeUp100>
-            <a href="https://www.buymeacoffee.com/puckyeu" className={styles.card}>
-              <h2 className={inter.className}>
-                Buy Me A Coffee <span>-&gt;</span>
-              </h2>
-              <p className={inter.className}>
-                You can buy me a coffee if you want to support me! I would really appreciate it!
-              </p>
-            </a>
-          </FadeUp100>
-
-          <FadeUp200>
-            <a href="https://github.com/PuckyEU" className={styles.card}>
-              <h2 className={inter.className}>
-                Github <span>-&gt;</span>
-              </h2>
-              <p className={inter.className}>
-                Take a look at my Github! You can find all of my projects there. This website is also open source, you can find it there too!
-              </p>
-            </a>
-          </FadeUp200>
-
-          <FadeUp300>
-            <a href="https://www.linkedin.com/in/lukassobotik/" className={styles.card}>
-              <h2 className={inter.className}>
-                LinkedIn <span>-&gt;</span>
-              </h2>
-              <p className={inter.className}>
-                Here you can find my LinkedIn profile. You can also contact me there.
-              </p>
-            </a>
-          </FadeUp300>
+          <a href="https://github.com/PuckyEU/puckyeu.github.io" data-aos="fade-up" className={styles.card}>
+            <h2 className={inter.className}>
+              Source Code <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Here you can find all of my source code for this website. If you have any questions or suggestions, feel free to contact me!
+            </p>
+          </a>
+          <a href="https://www.buymeacoffee.com/puckyeu" data-aos="fade-up" data-aos-delay="100" className={styles.card}>
+            <h2 className={inter.className}>
+              Buy Me A Coffee <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              You can buy me a coffee if you want to support me! I would really appreciate it!
+            </p>
+          </a>
+          <a href="https://github.com/PuckyEU" data-aos="fade-up" data-aos-delay="200" className={styles.card}>
+            <h2 className={inter.className}>
+              Github <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Take a look at my Github! You can find all of my projects there. This website is also open source, you can find it there too!
+            </p>
+          </a>
+          <a href="https://www.linkedin.com/in/lukassobotik/" data-aos="fade-up" data-aos-delay="300" className={styles.card}>
+            <h2 className={inter.className}>
+              LinkedIn <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Here you can find my LinkedIn profile. You can also contact me there.
+            </p>
+          </a>
         </div>
       </main>
     </>
