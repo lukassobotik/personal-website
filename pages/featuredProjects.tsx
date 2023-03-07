b856753c48dbe49c78dc00e5b21ddc0128beeff5
PuckyEU <osmset@gmail.com>
2023-03-07 20:50:22 +0100

    Featured Projects order change

--- pages/featuredProjects.tsx
+++ pages/featuredProjects.tsx
@@ -14,23 +14,7 @@ export default function FeaturedProjects() {
     return (
         <>
             <div className={styles.featured_projects}>
-                <Link href="/project/TaskLooper" data-aos="fade-left" className={styles.featured_project}>
-                    <div className={styles.featured_project_description}>
-                        <h2>TaskLooper</h2>
-                        <h4>
-                            Java, Android, SQLite
-                        </h4>
-                        <p>
-                            TaskLooper is an android app where you manage daily tasks you want to complete.
-                        </p>
-                        <p className={styles.extra_description}>
-                            Although it is similar to my other repository Repeaty, I have slightly different plans and goals for it.
-                        </p>
-                    </div>
-                    <Image src="/images/TaskLooper/TaskLooper.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
-                </Link>
                 <Link href="/project/MyFavMovies" data-aos="fade-right" className={styles.featured_project}>
-                    <Image src="/images/MyFavMovies/MyFavMovies.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
                     <div className={styles.featured_project_description}>
                         <h2>MyFavMovies</h2>
                         <h4>
@@ -43,8 +27,10 @@ export default function FeaturedProjects() {
                             It uses the The Movie Database API to fetch the movies.
                         </p>
                     </div>
+                    <Image src="/images/MyFavMovies/MyFavMovies.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
                 </Link>
                 <Link href="/project/myQuotes" data-aos="fade-left" className={styles.featured_project}>
+                    <Image src="/images/myQuotes/myQuotes.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
                     <div className={styles.featured_project_description}>
                         <h2>MyQuotes</h2>
                         <h4>
@@ -57,10 +43,8 @@ export default function FeaturedProjects() {
                             This was my first completed Android application that I published on the Google Play Store.
                         </p>
                     </div>
-                    <Image src="/images/myQuotes/myQuotes.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
                 </Link>
                 <Link href="/project/vsem-schedule-change-notifier" data-aos="fade-right" className={styles.featured_project}>
-                    <Image src="/images/schedule-change-notifier/VŠEM.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
                     <div className={styles.featured_project_description}>
                         <h2>VŠEM Schedule Change Notifier</h2>
                         <h4>
@@ -71,6 +55,22 @@ export default function FeaturedProjects() {
                         </p>
                         <p className={styles.extra_description}>
                             The application is built using Java, JavaMail and Jsoup. It is deployed on a Raspberry Pi and runs on a Cron job.
+                        </p>
+                    </div>
+                    <Image src="/images/schedule-change-notifier/VŠEM.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                </Link>
+                <Link href="/project/TaskLooper" data-aos="fade-left" className={styles.featured_project}>
+                    <Image src="/images/TaskLooper/TaskLooper.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                    <div className={styles.featured_project_description}>
+                        <h2>TaskLooper</h2>
+                        <h4>
+                            Java, Android, SQLite
+                        </h4>
+                        <p>
+                            TaskLooper is an android app where you manage daily tasks you want to complete.
+                        </p>
+                        <p className={styles.extra_description}>
+                            Although it is similar to my other repository Repeaty, I have slightly different plans and goals for it.
                         </p>
                     </div>
                 </Link>
