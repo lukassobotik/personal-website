4441e6269d92b0c65c6e6289f88372acec57f05f
PuckyEU <osmset@gmail.com>
2023-03-19 18:48:07 +0100

    Featured Projects Reorder

--- pages/featuredProjects.tsx
+++ pages/featuredProjects.tsx
@@ -14,23 +14,7 @@ export default function FeaturedProjects() {
     return (
         <>
             <div className={styles.featured_projects}>
-                <Link href="/project/MyFavMovies" data-aos="fade-right" className={styles.featured_project}>
-                    <div className={styles.featured_project_description}>
-                        <h2>MyFavMovies</h2>
-                        <h4>
-                            JavaScript, React, Firebase
-                        </h4>
-                        <p>
-                            MyFavMovies is a website made for finding movies and adding them to your favorites.
-                        </p>
-                        <p className={styles.extra_description}>
-                            It uses the The Movie Database API to fetch the movies.
-                        </p>
-                    </div>
-                    <Image src="/images/MyFavMovies/MyFavMovies.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
-                </Link>
                 <Link href="/project/myQuotes" data-aos="fade-left" className={styles.featured_project}>
-                    <Image src="/images/myQuotes/myQuotes.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
                     <div className={styles.featured_project_description}>
                         <h2>MyQuotes</h2>
                         <h4>
@@ -41,6 +25,22 @@ export default function FeaturedProjects() {
                         </p>
                         <p className={styles.extra_description}>
                             This was my first completed Android application that I published on the Google Play Store.
+                        </p>
+                    </div>
+                    <Image src="/images/myQuotes/myQuotes.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                </Link>
+                <Link href="/project/MyFavMovies" data-aos="fade-right" className={styles.featured_project}>
+                    <Image src="/images/MyFavMovies/MyFavMovies.png" alt="" width={150} height={150} className={styles.featured_project_image}/>
+                    <div className={styles.featured_project_description}>
+                        <h2>MyFavMovies</h2>
+                        <h4>
+                            JavaScript, React, Firebase
+                        </h4>
+                        <p>
+                            MyFavMovies is a website made for finding movies and adding them to your favorites.
+                        </p>
+                        <p className={styles.extra_description}>
+                            It uses the The Movie Database API to fetch the movies.
                         </p>
                     </div>
                 </Link>
