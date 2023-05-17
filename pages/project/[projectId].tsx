7181f3714eebcbfa6841ab2958b15fb7f2e4b6d1
Pucky <osmset@gmail.com>
2023-05-17 20:41:04 +0200

    Licenses

--- pages/project/[projectId].tsx
+++ pages/project/[projectId].tsx
@@ -1,6 +1,5 @@
-import { useRouter } from "next/router"
+import {useRouter} from "next/router"
 import {useEffect, useState} from "react";
-import {it} from "node:test";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import Navbar from "../../navbar";
