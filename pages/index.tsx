19f838c340d2b507eaee87c37d3f49efba5e5150
Pucky <osmset@gmail.com>
2023-06-17 14:54:06 +0200

    Title Page Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -249,63 +249,110 @@ export default function Home() {
   }, [router.query.tab]);
 
   return (
-    <>
-      <Head>
-        <title>Lukáš Sobotík</title>
-      </Head>
-      <main className={styles.main}>
-        <Image src="/images/banner.jpg" alt="" width={2000} height={300} className={styles.banner}/>
-        <div className={styles.splitter}>
-          <div className={styles.profile_info}>
-            <div className={styles.logo_parent}>
-              <Image src="/images/logo.jpg" alt="" width={300} height={300} className={styles.logo}/>
+      <>
+        <Head>
+          <title>Lukáš Sobotík</title>
+        </Head>
+        <main className={styles.main}>
+          <div className={styles.header}>
+            <div className={styles.header_name}>Lukáš Sobotík</div>
+            <div className={styles.header_tabs}>
+              <Link href="/?tab=home" className={styles.header_tab}>
+                  <h3 className={inter.className}>
+                      Home <span>-&gt;</span>
+                  </h3>
+              </Link>
+              <Link href="/?tab=projects" className={styles.header_tab}>
+                  <h3 className={inter.className}>
+                      Projects <span>-&gt;</span>
+                  </h3>
+              </Link>
+              <Link href="/?tab=contact" className={styles.header_tab}>
+                  <h3 className={inter.className}>
+                      Contact <span>-&gt;</span>
+                  </h3>
+              </Link>
             </div>
-            <p className={styles.profile_name_main}>Lukáš Sobotík</p>
-            <p className={styles.profile_name}>PuckyEU</p>
-            <p className={styles.bio}>At Akademie VŠEM, I&#39;m a student majoring in economics.
-              My interest is programming, thus I&#39;m open to any job or internship opportunities.
-              I am now studying Java, and I have minor knowledge of JavaScript and C#.
-              Since December 2021, I&#39;ve been actively learning programming.
-              Since then, I have created Android applications and websites.
-            </p>
-            <p className={styles.bio}>I don&#39;t currently have any professional expertise in software development, but I&#39;d really like to. </p>
-            <p className={styles.bio}>Age: {calculateAge()}</p>
-            <p className={styles.bio}>Job Status: Looking for a job</p>
-            <p className={styles.profile_link}><a href="/resume/junior-java-developer.pdf">Resume</a></p>
-            <p className={styles.profile_link}><a href="https://github.com/PuckyEU/puckyeu.github.io">Source Code</a></p>
-            <p className={styles.profile_link}><a href="https://www.buymeacoffee.com/puckyeu">BuyMeACoffee</a></p>
-            <p className={styles.profile_link}><a href="https://github.com/PuckyEU">Github</a></p>
-            <p className={styles.profile_link}><a href="https://www.linkedin.com/in/lukassobotik/">LinkedIn</a></p>
-            <p className={styles.profile_link}><a href="https://play.google.com/store/apps/dev?id=6212701078283176937">Google Play</a></p>
-            <p className={styles.profile_link}><a href="https://twitter.com/PuckyEU">Twitter</a></p>
-            <p className={styles.profile_link}><a href="https://youtube.com/@PuckyEU">YouTube</a></p>
-            <p className={styles.profile_link}><a href="https://www.codewars.com/users/PuckyEU">CodeWars</a></p>
           </div>
-          {/*Links*/}
-          <div className={styles.description}>
-            <div className={styles.links}>
-                <span className={styles.navbar_item} onClick={homeClick}>
-                    <h4>
-                        Home
-                    </h4>
-                </span>
-                <span className={styles.navbar_item} onClick={projectsClick}>
-                    <h4>
-                        Projects
-                    </h4>
-                </span>
-                <span className={styles.navbar_item} onClick={contactClick}>
-                    <h4>
-                        Contact
-                    </h4>
-                </span>
+          <div className={styles.content}>
+            <div className={styles.main_image}>.</div>
+            <div className={styles.content_main}>
+              <div className={styles.content_main_line}>Hi, I&apos;m Lukáš</div>
+              <div className={styles.content_main_short_summary}>I am a 16-year-old student from the Czech Republic.
+                My interest is programming, thus I&#39;m open to any job or internship opportunities.
+                I am now studying Java, and I have minor knowledge of JavaScript and C#.
+                Since December 2021, I&#39;ve been actively programming.</div>
+              <div className={styles.main_contact}>
+                <div className={styles.main_contact_header}>Contact</div>
+                <div className={styles.main_contact_item}><Link href={"https://linkedin.com/in/lukassobotik/"}>LinkedIn</Link></div>
+                <div className={styles.main_contact_item}><Link href={"mailto://jobs@lukassobotik.dev"}>Email</Link></div>
+                <div className={styles.main_contact_item}><Link href={"https://github.com/PuckyEU"}>Github</Link></div>
+                <div className={styles.main_contact_item}><Link href={"https://www.buymeacoffee.com/puckyeu"}>Buy me a coffee</Link></div>
+              </div>
             </div>
-        </div>
-        </div>
-        <div className={styles.tab}>
-          {tabContent}
-        </div>
-      </main>
-    </>
+          </div>
+        </main>
+      </>
   )
+
+  // return (
+  //   <>
+  //     <Head>
+  //       <title>Lukáš Sobotík</title>
+  //     </Head>
+  //     <main className={styles.main}>
+  //       <Image src="/images/banner.jpg" alt="" width={2000} height={300} className={styles.banner}/>
+  //       <div className={styles.splitter}>
+  //         <div className={styles.profile_info}>
+  //           <div className={styles.logo_parent}>
+  //             <Image src="/images/logo.jpg" alt="" width={300} height={300} className={styles.logo}/>
+  //           </div>
+  //           <p className={styles.profile_name_main}>Lukáš Sobotík</p>
+  //           <p className={styles.profile_name}>PuckyEU</p>
+  //           <p className={styles.bio}>At Akademie VŠEM, I&#39;m a student majoring in economics.
+  //             My interest is programming, thus I&#39;m open to any job or internship opportunities.
+  //             I am now studying Java, and I have minor knowledge of JavaScript and C#.
+  //             Since December 2021, I&#39;ve been actively learning programming.
+  //             Since then, I have created Android applications and websites.
+  //           </p>
+  //           <p className={styles.bio}>I don&#39;t currently have any professional expertise in software development, but I&#39;d really like to. </p>
+  //           <p className={styles.bio}>Age: {calculateAge()}</p>
+  //           <p className={styles.bio}>Job Status: Looking for a job</p>
+  //           <p className={styles.profile_link}><a href="/resume/junior-java-developer.pdf">Resume</a></p>
+  //           <p className={styles.profile_link}><a href="https://github.com/PuckyEU/puckyeu.github.io">Source Code</a></p>
+  //           <p className={styles.profile_link}><a href="https://www.buymeacoffee.com/puckyeu">BuyMeACoffee</a></p>
+  //           <p className={styles.profile_link}><a href="https://github.com/PuckyEU">Github</a></p>
+  //           <p className={styles.profile_link}><a href="https://www.linkedin.com/in/lukassobotik/">LinkedIn</a></p>
+  //           <p className={styles.profile_link}><a href="https://play.google.com/store/apps/dev?id=6212701078283176937">Google Play</a></p>
+  //           <p className={styles.profile_link}><a href="https://twitter.com/PuckyEU">Twitter</a></p>
+  //           <p className={styles.profile_link}><a href="https://youtube.com/@PuckyEU">YouTube</a></p>
+  //           <p className={styles.profile_link}><a href="https://www.codewars.com/users/PuckyEU">CodeWars</a></p>
+  //         </div>
+  //         {/*Links*/}
+  //         <div className={styles.description}>
+  //           <div className={styles.links}>
+  //               <span className={styles.navbar_item} onClick={homeClick}>
+  //                   <h4>
+  //                       Home
+  //                   </h4>
+  //               </span>
+  //               <span className={styles.navbar_item} onClick={projectsClick}>
+  //                   <h4>
+  //                       Projects
+  //                   </h4>
+  //               </span>
+  //               <span className={styles.navbar_item} onClick={contactClick}>
+  //                   <h4>
+  //                       Contact
+  //                   </h4>
+  //               </span>
+  //           </div>
+  //       </div>
+  //       </div>
+  //       <div className={styles.tab}>
+  //         {tabContent}
+  //       </div>
+  //     </main>
+  //   </>
+  // )
 }
