b6066fbbe45614ada92fc744b35f32bbabbec68c
lukassobotik <osmset@gmail.com>
2023-11-02 20:59:14 +0100

    Fixed Contact Page AOS Delays

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -21,10 +21,10 @@ export default function Contact() {
                 <Navbar/>
                 <h1 className={styles.contact_header} data-aos="fade-up">Contact</h1>
                 <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="50"><Link className={styles.url} href="/resume/junior-java-developer.pdf">Resume (Java)</Link></div>
-                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="50"><Link className={styles.url} href="/resume/web-developer.pdf">Resume (Web Dev)</Link></div>
-                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="100"><Link className={styles.url} href="https://github.com/lukassobotik/personal-website">Source Code</Link></div>
-                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="150"><Link className={styles.url} href="https://www.buymeacoffee.com/lukassobotik">Buy me a coffee</Link></div>
-                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="200"><Link className={styles.url} href="https://github.com/lukassobotik">Github</Link></div>
+                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="100"><Link className={styles.url} href="/resume/web-developer.pdf">Resume (Web Dev)</Link></div>
+                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="150"><Link className={styles.url} href="https://github.com/lukassobotik/personal-website">Source Code</Link></div>
+                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="200"><Link className={styles.url} href="https://www.buymeacoffee.com/lukassobotik">Buy me a coffee</Link></div>
+                <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="250"><Link className={styles.url} href="https://github.com/lukassobotik">Github</Link></div>
                 <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="300"><Link className={styles.url} href="https://www.linkedin.com/in/lukassobotik/">LinkedIn</Link></div>
                 <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="350"><Link className={styles.url} href="https://www.freelancer.com/u/lukassobotik">Freelancer</Link></div>
                 <div className={styles.main_contact_item} data-aos="fade-up" data-aos-delay="400"><Link className={styles.url} href="https://play.google.com/store/apps/dev?id=6212701078283176937">Google Play</Link></div>
