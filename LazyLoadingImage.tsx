9e2912da1301635e3a7a65f6ca2c493b6c0805d8
lukassobotik <osmset@gmail.com>
2023-12-16 22:10:15 +0100

    LazyLoadingImage.tsx Documentation

--- LazyLoadingImage.tsx
+++ LazyLoadingImage.tsx
@@ -1,10 +1,32 @@
 import { useState } from 'react';
 import Image from 'next/image';
 
-const LazyLoadingImage = ({ src, alt, width, height }: { src : string, alt : string, width : number, height : number }) => {
+/**
+ * `LazyLoadingImage` is a functional component in React that renders an image with lazy loading.
+ * It takes four props: `src`, `alt`, `width`, and `height`.
+ * `src` is a string that represents the source URL of the image.
+ * `alt` is a string that provides alternative text for the image.
+ * `width` is a number that sets the width of the image.
+ * `height` is a number that sets the height of the image.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.src - The source URL of the image.
+ * @param {string} props.alt - The alternative text for the image.
+ * @param {number} props.width - The width of the image.
+ * @param {number} props.height - The height of the image.
+ * @returns {JSX.Element} The Image component with the specified properties and loading state.
+ */
+const LazyLoadingImage = ({ src, alt, width, height }: { src : string, alt : string, width : number, height : number }): JSX.Element => {
     const [isLoaded, setIsLoaded] = useState(false);
 
-    const handleImageLoad = () => {
+    /**
+     * `handleImageLoad` is a function that sets the `isLoaded` state to true.
+     * This function is triggered when the image has finished loading.
+     * It does not take any parameters and does not return any value.
+     *
+     * @returns {void}
+     */
+    const handleImageLoad = (): void => {
         setIsLoaded(true);
     };
 
