964a2dd57fc2a2e1942d738c662a7f3cc3b4696f
lukassobotik <osmset@gmail.com>
2023-12-16 22:12:57 +0100

    FetchProjects.tsx Documentation

--- pages/FetchProjects.tsx
+++ pages/FetchProjects.tsx
@@ -8,6 +8,16 @@ import ProjectOverview from "../ProjectOverview";
 export default function FetchProjects({onlyFeatured}: {onlyFeatured: boolean}) {
     const [projects, setProjects] = useState<Project[]>();
 
+    /**
+     * This `useEffect` hook is responsible for fetching the projects based on the `onlyFeatured` prop.
+     * If `onlyFeatured` is true, it fetches only the featured projects, otherwise it fetches all projects.
+     * It then fetches the details of each project by its id and sets the `projects` state with the fetched projects.
+     * It also initializes the AOS (Animate On Scroll) library.
+     *
+     * This hook runs whenever the `onlyFeatured` prop changes.
+     *
+     * @returns {void}
+     */
     useEffect(() => {
         fetchProjectsByFeatured(onlyFeatured).then((projects) => {
             const ids: string[] = projects.map((project) => project.id);
