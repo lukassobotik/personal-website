cbb2298db4199e1b43d44dff5e574105cd664a9c
PuckyEU <osmset@gmail.com>
2024-05-29 19:37:45 +0200

    #50

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,7 @@ export default function Home() {
             <div className={styles.content_main}>
               <div className={styles.content_main_line} id={"content_main_line"} onMouseEnter={decryptEffect}>Hi, I&apos;m Lukáš</div>
               <div className={styles.content_main_short_summary} data-aos="fade-up">I am a {calculateAge()}-year-old student from the Czech Republic.
-                My interest is programming, so I&#39;m open to any job or internship opportunities.
+                My interest is programming, and currently, I&#39;m open to any job or internship opportunities.
                 I am now learning Java, and I have minor knowledge of JavaScript and C#.
                 Since December 2021, I&#39;ve been actively programming.
               </div>
